4b220a8cb9fbe36473877256a27e3118a5d19e7b
PARTHKOTADIYAA <166747238+PARTHKOTADIYAA@users.noreply.github.com>
2026-07-14 18:04:03 -0400

    Refactor variables for Lambda module
    Removed several variables and added new ones for Lambda configuration.

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,19 +1,13 @@
-variable "service_name" { type = string }
-variable "environment" { type = string }
-variable "lambda_role_arn" { type = string }
-variable "source_dir" { type = string }
-
-variable "environment_variables" {
-  type    = map(string)
-  default = {}
-}
-
-variable "vpc_subnet_ids" {
-  type    = list(string)
-  default = []
-}
-
-variable "vpc_security_group_ids" {
-  type    = list(string)
-  default = []
+variable "stage" { type = string }
+variable "function_name" { type = string }
+variable "lambda_iam_execution_role_arn" { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "lambda_security_group_id" { type = string }
+variable "central_api_gateway_execution_arn" { type = string }
+variable "rds_endpoint" { type = string }
+variable "db_user" { type = string }
+variable "db_name" { type = string }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
 }
