e77a072e1b758a4589fb0b3d72b5bdfc168d508a
PARTHKOTADIYAA <166747238+PARTHKOTADIYAA@users.noreply.github.com>
2026-07-14 18:04:24 -0400

    Remove function ARN output from outputs.tf
    Removed output for function ARN from outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -2,10 +2,6 @@ output "function_name" {
   value = aws_lambda_function.this.function_name
 }
 
-output "function_arn" {
-  value = aws_lambda_function.this.arn
-}
-
 output "invoke_arn" {
   value = aws_lambda_function.this.invoke_arn
 }
