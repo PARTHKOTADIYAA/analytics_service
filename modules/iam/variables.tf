c104d5ecb5a21a4a09d7e49f8169f7e999912f61
PARTHKOTADIYAA <166747238+PARTHKOTADIYAA@users.noreply.github.com>
2026-07-14 18:01:56 -0400

    Refactor IAM variables in variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,7 +1,8 @@
-variable "service_name" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
+variable "role_name" { type = string }
+variable "policy_name" { type = string }
+variable "apigw_role_name" { type = string }
+variable "stage" { type = string }
+variable "region" { type = string }
+variable "account_id" { type = string }
+variable "rds_resource_id" { type = string }
+variable "db_user" { type = string }
