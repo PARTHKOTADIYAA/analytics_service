9785aaf6657baa868e712ab8902e373f58620f7e
PARTHKOTADIYAA <166747238+PARTHKOTADIYAA@users.noreply.github.com>
2026-07-14 17:53:32 -0400

    Refactor API Gateway OPTIONS method resources

--- modules/cors/main.tf
+++ modules/cors/main.tf
@@ -1,27 +1,25 @@
-resource "aws_api_gateway_method" "options_events" {
+resource "aws_api_gateway_method" "options_method" {
   rest_api_id   = var.rest_api_id
   resource_id   = var.resource_id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "options_mock" {
-  rest_api_id = var.rest_api_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options_events.http_method
-  type        = "MOCK"
-
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id             = var.rest_api_id
+  resource_id             = var.resource_id
+  http_method             = aws_api_gateway_method.options_method.http_method
+  type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
 
-resource "aws_api_gateway_method_response" "options_200" {
+resource "aws_api_gateway_method_response" "options_response" {
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options_events.http_method
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = "200"
-
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true
     "method.response.header.Access-Control-Allow-Methods" = true
@@ -29,17 +27,14 @@ resource "aws_api_gateway_method_response" "options_200" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "options_200" {
+resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options_events.http_method
-  status_code = aws_api_gateway_method_response.options_200.status_code
-
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_response.status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'${var.allowed_origin}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'${join(",", var.allowed_methods)}'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.allowed_origins}'"
   }
-
-  depends_on = [aws_api_gateway_integration.options_mock]
 }
